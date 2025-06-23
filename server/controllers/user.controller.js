a76f3f1aace66d78c61b97a4e222749e3d9b2a17
Vinay Jhalani <VinayJhalani.dev@gmail.com>
2025-06-24 01:18:58 +0530

    Logout Func

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -91,7 +91,7 @@ export const logout = async (req, res) => {
     res.clearCookie("token", {
       httpOnly: true,
       secure: true,
-      sameSite: "Strict",
+      sameSite: "None",
       path: "/",
     });
 
